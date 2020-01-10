cc748138f876ef43f73fce3e4809fbac9776a5b7
Richard <richrdhw@gmail.com>
2020-01-10 10:02:50 -0500

    Added char controller and game manager

---
+++ Test/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    public string name = "Richard";
+    public int health = 200;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
